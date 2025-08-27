0fe181f0c234323dc42faa27df6644605dfba2c4
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-27 21:31:55 +0430

    dispaly word and its details

--- main.py
+++ main.py
@@ -16,3 +16,19 @@ def load_dictionary():
 def save_dictionary(dic):
     with open(FILE_NAME, "w", encoding="utf-8") as f:
         json.dump(dic, f, ensure_ascii=False, indent=4)
+
+
+# Display a word and its details
+def display_word(dic, word):
+    if word in dic:
+        print(f"\nWord: {word}")
+        print(f"Meaning: {dic[word]['meaning']}")
+        print(f"Example: {dic[word]['example']}")
+        print(
+            f"Synonyms: {', '.join(dic[word]['synonyms']) if dic[word]['synonyms'] else 'None'}"
+        )
+        print(
+            f"Antonyms: {', '.join(dic[word]['antonyms']) if dic[word]['antonyms'] else 'None'}"
+        )
+    else:
+        print("Word not found!")
