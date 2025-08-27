d358c65c7aa881ffaf492ffa0f67cf9e5562655a
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-27 21:30:31 +0430

    load dictionary from file

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import json
 import os
 
 FILE_NAME = "dictionary.json"
+
+
+# Load dictionary from file
+def load_dictionary():
+    if os.path.exists(FILE_NAME):
+        with open(FILE_NAME, "r", encoding="utf-8") as f:
+            return json.load(f)
+    return {}
